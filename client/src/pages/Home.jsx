2059e4cd614a2af64dd6861e1cdc1b80c8d6f055
AgamjotSB <agamjot.sb@gmail.com>
2025-08-05 10:11:08 +0530

    Tailwind setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-amber-300 flex items-center">
+      Home
+      <h1 className="text-4xl font-bold text-blue-800">Tailwind is working</h1>
+    </div>
+  );
 }
 
 export default Home;
